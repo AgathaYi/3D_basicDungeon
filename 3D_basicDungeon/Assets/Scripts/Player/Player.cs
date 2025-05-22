b554b85484d07701eb6ac6835d2d739cfac9fa00
AgathaYi <tjsfid1030@naver.com>
2025-05-22 15:37:53 +0900

    add: icon 이미지 파일 추가 및 프리팹수정 ( #8 )
    icon 이미지 파일 추가

--- 3D_basicDungeon/Assets/Scripts/Player/Player.cs
+++ 3D_basicDungeon/Assets/Scripts/Player/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     public PlayerController controller;
     public PlayerCondition condition;
 
-    public Action addItem;
+    public ItemData itemdata; // 획득한 아이템 정보
+    public System.Action addItem;
+
+    public Transform dropPosition; // 아이템 버릴 위치
 
     private void Awake()
     {
